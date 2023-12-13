4f1f2aa26937916663de729b73964e0b4f81d798
Dobos Dominik (SZF_2023_2025) <dobos.dominik@diak.szamalk-szalezi.hu>
2023-12-13 14:09:15 +0100

    Helyszin algoritmusanak fejlesztese

--- jatekter.py
+++ jatekter.py
@@ -5,11 +5,31 @@ jatekos = Jatekos.Jatekos(5,0)
 parancsokLista = ["megy","eszik","felvesz","hasznal"]
 
 def jatekMenet():
-    helyszin1()
+    hanyadik = 1
+    while hanyadik < 14:
+        if helyszin(hanyadik):
+            hanyadik += 1
 
 
-def helyszin1():
-    szoveg.szovegListaba(1)
-    
 
+
+
+def felhasznaloBekeres():
+    return input("Parancssor: ")
+
+
+def helyszin(hanyadik):
+    tovabbhalad = False
+    szoveg.szovegListaba(hanyadik)
+    parancs = felhasznaloBekeres()
+    parancssor = parancs.split(" ")
+    helyszin = ["epulet"]
+    while not tovabbhalad:
+        if parancssor[0] == parancsokLista[0]:
+            if len(parancssor) == 2:
+                if parancssor[1] == helyszin[hanyadik-1]:
+                    tovabbhalad = True
+                    return tovabbhalad
+        else:
+            parancs = felhasznaloBekeres()
 
