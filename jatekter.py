02abc8b555f1fa2da23e9d20327a578cc087e56d
Unk0wn1337 <kelogames@gmail.com>
2023-12-13 13:37:59 +0100

    ParancsokLista megadas, targyLista

--- jatekter.py
+++ jatekter.py
@@ -1,3 +1,13 @@
 import Jatekos
 targyakLista = []
 jatekos = Jatekos.Jatekos(5,0)
+parancsokLista = ["megy","eszik","felvesz","hasznal"]
+
+def jatekMenet():
+    helyszin1()
+
+
+def helyszin1():
+    pass
+
+
